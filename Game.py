0e7c2796d9e94d117442c73730c3bfcaccb3ce38
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-05-12 20:36:07 +0200

    feat: add game logic

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import pygame
 from Player import Player
+from Obstacle import Obstacle
 
 class Game:
     
@@ -7,17 +8,55 @@ class Game:
     clock = pygame.time.Clock()
     running = True
     player = Player()
+    obstacles = []
+    obstacles_gap = 250
+    game_speed = 2
+    
+    distance = 0
+    lose_count = 0
     
     def __init__(self):
         pygame.init()
         
+    def lose(self):
+        self.lose_count += 1
+        self.player = Player()
+        self.distance = 0
+        self.obstacles = []
+        
     def draw(self):
+        obstacles = self.obstacles
+        
+        # clear screen
         self.screen.fill((0, 0, 0))
+
+        # draw game objects
+        for obstacle in obstacles:
+            obstacle.draw(self.screen)
+        
         self.player.draw(self.screen)
+        
+        # draw game text score
+        font = pygame.font.Font(None, 36)
+        text = font.render(str(self.distance), 1, (255, 255, 255))
+        textpos = text.get_rect(centerx=self.screen.get_width()/2)
+        self.screen.blit(text, textpos)
+        
+        # update screen
         pygame.display.flip()
         
+        
     def update(self):
-        self.player.update()
+        obstacles = self.obstacles
+
+        # update game objects
+        for obstacle in obstacles:
+            if obstacle.update(self.game_speed) == False:
+                self.obstacles.remove(obstacle)
+        
+        if self.player.update() == False:
+            self.lose()
+                   
     
     def run(self):
         while self.running:
@@ -30,8 +69,13 @@ class Game:
             if keys[pygame.K_SPACE]:
                 self.player.jump()
 
-            self.draw()
             self.update()
+            self.draw()
+            
+            if self.distance%self.obstacles_gap == 0:
+                self.obstacles.append(Obstacle())
+                
+            self.distance += self.game_speed
 
 game = Game()
 game.run()
