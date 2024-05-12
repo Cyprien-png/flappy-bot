9f7f290079ad2ea6ba0cbcebd9f94ee0ee4a7148
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-05-12 20:28:28 +0200

    fix: spawning location of the obstacles

--- Obstacle.py
+++ Obstacle.py
@@ -1,19 +1,20 @@
 import pygame
+import random
 
 class Obstacle:
     
     def __init__(self):
-        self.position_x = pygame.display.get_surface().get_size()[0] - 100
+        self.position_x = pygame.display.get_surface().get_size()[0]
         self.width = 50
         self.hole_size = 200
-        self.hole_height = 100
+        self.hole_height = random.randint(0, pygame.display.get_surface().get_size()[1] - self.hole_size)
     
     def draw(self, screen):
         pygame.draw.rect(screen, (0, 255, 0), (self.position_x, 0, self.width, self.hole_height))
         pygame.draw.rect(screen, (0, 255, 0), (self.position_x, self.hole_height + self.hole_size, self.width, pygame.display.get_surface().get_size()[1]))
         
-    def update(self):
-        self.position_x -= 3
+    def update(self, velocity):
+        self.position_x -= velocity
         return False if self.position_x + self.width < 0 else True
         
      
